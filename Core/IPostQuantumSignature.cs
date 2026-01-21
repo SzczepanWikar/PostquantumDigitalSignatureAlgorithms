a6122bfd3e80863e7683594b202b7cd60fc669ea
Szczepan Wikar <szczepanwikar@gmail.com>
2026-01-21 19:59:51 +0100

    Implement FIPD 204 Facade

---
+++ Core/IPostQuantumSignature.cs
@@ -0,0 +1,10 @@
+﻿namespace Core
+{
+    public interface IPostQuantumSignature
+    {
+        (byte[] PublicKey, byte[] PrivateKey) GenerateKeyPair();
+        byte[] Sign(ReadOnlySpan<byte> message);
+        bool Verify(ReadOnlySpan<byte> message, ReadOnlySpan<byte> signature);
+        (byte[] PublicKey, byte[] PrivateKey) ExportKeys();
+    }
+}
---
+++ Core/PlatformImplementation/Fips205Facade.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Core.PlatformImplementation
+{
+    public sealed class Fips205Facade : IPostQuantumSignature
+    {
+        public (byte[] PublicKey, byte[] PrivateKey) ExportKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public (byte[] PublicKey, byte[] PrivateKey) GenerateKeyPair()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Sign(ReadOnlySpan<byte> message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Verify(ReadOnlySpan<byte> message, ReadOnlySpan<byte> signature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
