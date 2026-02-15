7e7ff8d4bb0c69af39f1f48b8e23a1462b378efd
Szczepan Wikar <szczepanwikar@gmail.com>
2026-02-15 11:57:39 +0100

    Pseudorandom Sampling

--- CrystalsDilithium/DilithiumFunctions.cs
+++ CrystalsDilithium/DilithiumFunctions.cs
@@ -32,33 +32,6 @@ namespace CrystalsDilithium
             return mod;
         }
 
-        public short[] SampleInBall(byte[] seed)
-        {
-            short[] c = new short[256];
-            Array.Fill(c, (short)0);
-
-            using Shake256 shake256 = new();
-            shake256.AppendData(seed);
-
-            byte[] hash = shake256.Read(8);
-            BitArray h = new BitArray(hash);
-
-            for (int i = 256 - _parameters.Tau; i < 256; i++)
-            {
-                int j = shake256.Read(1)[0];
-
-                while (j > i)
-                {
-                    j = shake256.Read(1)[0];
-                }
-
-                c[i] = c[j];
-                c[j] = (short)Math.Pow(-1, h[i + _parameters.Tau - 256].ToInt32());
-            }
-
-            return c;
-        }
-
         public (int r1, int r0) Power2Round(int r)
         {
             Debug.Assert(r >= 0, "Input must be non-negative.");
---
+++ CrystalsDilithium/PseudoRandomSampling.cs
@@ -0,0 +1,171 @@
+﻿using Core.Helpers;
+using System.Collections;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace CrystalsDilithium
+{
+    internal sealed class PseudoRandomSampling
+    {
+        private readonly DilithiumParameters _parameters;
+        private readonly BitAlgorithms _bitAlgorithms;
+
+        public PseudoRandomSampling(DilithiumParameters parameters)
+        {
+            _parameters = parameters;
+            _bitAlgorithms = new BitAlgorithms(parameters);
+        }
+
+        public short[] SampleInBall(byte[] rho)
+        {
+            short[] c = new short[256];
+            Array.Fill(c, (short)0);
+
+            using Shake256 shake256 = new();
+            shake256.AppendData(rho);
+
+            byte[] hash = shake256.Read(8);
+            BitArray h = new BitArray(hash);
+
+            for (int i = 256 - _parameters.Tau; i < 256; i++)
+            {
+                int j = shake256.Read(1)[0];
+
+                while (j > i)
+                {
+                    j = shake256.Read(1)[0];
+                }
+
+                c[i] = c[j];
+                c[j] = (short)Math.Pow(-1, h[i + _parameters.Tau - 256].ToInt32());
+            }
+
+            return c;
+        }
+
+        public int[] RejNTTPoly(byte[] rho)
+        {
+            short j = 0;
+
+            using Shake256 shake256 = new();
+            shake256.AppendData(rho);
+
+            int[] aDash = new int[256];
+
+            while (j < 256)
+            {
+                byte[] s = shake256.Read(3);
+                int? coeff = _bitAlgorithms.CoeffFromThreeBytes(s[0], s[1], s[2]);
+                
+                if(coeff.HasValue)
+                {
+                    aDash[j] = coeff.Value;
+                    j++;
+                }
+            }
+
+            return aDash;
+        }
+
+        public int[] RejBoundedPoly(byte[] rho)
+        {
+            short j = 0;
+
+            using Shake256 shake256 = new();
+            shake256.AppendData(rho);
+
+            int[] a = new int[256];
+
+            while(j < 256)
+            {
+                byte z = shake256.Read(1)[0];
+                int? z0 = _bitAlgorithms.CoeffFromHalfByte((byte)(z % 16));
+                int? z1 = _bitAlgorithms.CoeffFromHalfByte((byte)(z / 16));
+
+                if(z0.HasValue)
+                {
+                    a[j] = z0.Value;
+                    j++;
+                }
+
+                if(z1.HasValue && j < 256)
+                {
+                    a[j] = z1.Value;
+                    j++;
+                }
+            }
+
+            return a;
+        }
+
+        public int[][][] ExpandA(byte[] rho)
+        {
+            int[][][] aHat = InitAHatMatrix();
+
+            for(int r = 0; r < _parameters.AMatrixDimensions.K; r++)
+            {
+                for(int s = 0; s < _parameters.AMatrixDimensions.L; s++)
+                {
+                    byte[] rhoPrim = rho
+                        .Concat(_bitAlgorithms.IntegerToBytes(r, 1))
+                        .Concat(_bitAlgorithms.IntegerToBytes(s, 1))
+                        .ToArray();
+
+                    aHat[r][s] = RejNTTPoly(rhoPrim);
+                }
+            }
+
+            return aHat;
+        }
+
+        public (int[][]s1, int[][] s2) ExpandS(byte[] rho)
+        {
+            int[][] s1 = new int[_parameters.AMatrixDimensions.L][];
+            int[][] s2 = new int[_parameters.AMatrixDimensions.K][];
+       
+            for(int r = 0; r < _parameters.AMatrixDimensions.L; r++)
+            {
+                s1[r] = RejBoundedPoly(rho.Concat(_bitAlgorithms.IntegerToBytes(r, 1)).ToArray());
+            }
+
+            for(int r = 0; r < _parameters.AMatrixDimensions.K; r++)
+            {
+                s2[r] = RejBoundedPoly(rho.Concat(_bitAlgorithms.IntegerToBytes(r, 1)).ToArray());
+            }
+
+            return (s1, s2);
+        }
+
+        public int[][] ExpandMask(byte[] rho, int mi)
+        {
+            Debug.Assert(mi > 0, "Mask index must be positive.");
+
+            int[][] y = new int[_parameters.AMatrixDimensions.L][];
+            int c = 1 + BitLength.GetNumberBitLength(_parameters.Gamma1 - 1);
+
+            for (int r = 0; r < _parameters.AMatrixDimensions.L; r++)
+            {
+                byte[] rhoPrim = rho
+                    .Concat(_bitAlgorithms.IntegerToBytes(mi, 1))
+                    .Concat(_bitAlgorithms.IntegerToBytes(r, 1))
+                    .ToArray();
+
+                byte[] v = Shake256.HashData(rhoPrim, c);
+                y[r] = _bitAlgorithms.BitUnpack(v, _parameters.Gamma1 - 1, _parameters.Gamma1);
+            }
+
+            return y;
+        }
+        private int[][][] InitAHatMatrix()
+        {
+            int[][][] aHat = new int[_parameters.AMatrixDimensions.K][][];
+
+            for (int i = 0; i < _parameters.AMatrixDimensions.K; i++)
+            {
+                aHat[i] = new int[_parameters.AMatrixDimensions.L][];
+            }
+
+            return aHat;
+        }
+    }
+}
